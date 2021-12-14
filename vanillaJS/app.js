41f936ea2cc35de9fe61de46560045f5296d29ad
yeopto <gunyeop530@gmail.com>
2021-12-14 16:28:56 +0900

    2021-12-14 바닐라JS 챌린지 DAY2

--- vanillaJS/app.js
+++ vanillaJS/app.js
@@ -1,1 +1,14 @@
-alert("hi");
+const toBuy = ["potato", "tomate", "pizza"];
+console.log(toBuy);
+
+toBuy.push("kimbab");
+console.log(toBuy);
+
+const player = {
+    name: "yeop",
+    points: 10,
+    fat: true,
+};
+console.log(player);
+player.lastname = "potato";
+console.log(player);
